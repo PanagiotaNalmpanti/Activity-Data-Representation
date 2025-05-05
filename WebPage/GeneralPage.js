423bbf46b67a36a03098003a1350e6111bba5e64
Panagiota Nalmpanti <nspanagi@csd.auth.gr>
2025-05-05 20:54:09 +0300

    basic grid template of general page basic architecture of js page

---
+++ WebPage/GeneralPage.js
@@ -0,0 +1,23 @@
+document.addEventListener('DOMContentLoaded', () => {
+    chartCreation();
+});
+
+function chartCreation() {
+    // first sparql query
+    firstChart(); // need to pass the data
+
+    // second sparql query
+    secondChart(); // need to pass the data
+
+    // third sparql query
+    thirdChart(); // need to pass the data
+
+    //fourth sparql query
+    fourthChart(); // need to pass the data
+
+    // fifth sparql query
+    fifthChart(); // need to pass the data
+
+    // sixth sparql query
+    sixthChart(); // need to pass the data
+}
